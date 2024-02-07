0dacc5409656f6380f277f625c852c8e1bb6a9cd
Rukkiana <rukayyaabdulkadri@gmail.com>
2024-02-07 11:56:06 +0000

    first commit

---
+++ conditions/main.js
@@ -0,0 +1,60 @@
+// let userAge=prompt("enter your age");
+
+// if (userAge>=18){
+//     alert('you are old enough to drive');
+
+// }else{
+//     let ageLeft=18-Number(userAge)
+//     alert(`you are left with ${ageLeft} years to drive`);
+// }
+// const number=prompt('what number do you wish to know if it is even or odd?')
+// if(+number%2===0){
+//     alert('number is an even number')
+// } else{
+//     alert('number is an odd number');
+// }
+
+const myFirstArray=Array(10).fill('student')
+console.log(myFirstArray)
+const studentArray=['aisha',"salamat","titi"];
+console.log(studentArray)
+const numberArray=[2,2,3,1,8];
+console.log(numberArray);
+const mixedArray=(1, 2, 3,"fatimah","fareeda","rufaidah")
+console.log(mixedArray);
+const javaascript="javascript is bae"
+console.log(javaascript);
+console.log(javaascript.split('a'));
+console.log(studentArray .length);
+console.log(javaascript.lastIndexOf('x'));
+
+console.log(studentArray.lastIndexOf('a'));
+studentArray.push('bashira');
+console.log(studentArray);
+studentArray.unshift('halima');
+console.log(studentArray);
+studentArray.pop('bashira');
+console.log(studentArray);
+studentArray.shift('titi');
+console.log(studentArray);
+console.log(studentArray.reverse());
+studentArray[0]='maryam';
+console.log(studentArray);
+console.log(studentArray.indexOf('aisha'));
+console.log(javaascript.charAt(0));
+console.log(studentArray.concat(numberArray));
+console.log(Array.isArray(javaascript))
+console.log(studentArray.join())
+console.log(studentArray.join(" and "));
+// studentArray.splice(1)
+studentArray.splice (1,1,"nafisa","fatima" );
+console.log(studentArray);
+
+
+
+
+
+
+
+
+
